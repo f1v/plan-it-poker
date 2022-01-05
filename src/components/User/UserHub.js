320c238d319a3375c6a3f4cdc2e379215cb919a7
NMarini <nmarini@cainc.com>
2022-01-05 14:01:57 -0500

    created UserHub to display list of users

--- src/components/User/UserHub.js
+++ src/components/User/UserHub.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
+import Timer from '../../components/Timer';
 import _ from 'lodash';
 
 export const UserHub = ({ message }) => {
@@ -10,14 +11,20 @@ export const UserHub = ({ message }) => {
       <span>
         {player.name}: {player.vote !== 0 ? 'Voted' : '(...waiting)'}
       </span>
-      <span>[Timer]</span>
+      <Timer />
     </div>
   );
 
   const playersList = () => _.map(users, (user) => playerItem(user));
 
   const displayMessage = () => (
-    <div style={{ borderRadius: '5px', backgroundColor: 'lightblue' }}>
+    <div
+      style={{
+        borderRadius: '5px',
+        backgroundColor: 'lightblue',
+        marginBottom: '10px',
+      }}
+    >
       {message}
     </div>
   );
@@ -25,7 +32,18 @@ export const UserHub = ({ message }) => {
   return (
     <div style={{ paddingLeft: '20vw' }}>
       {displayMessage()}
-      <h5>Players: [TIMER] </h5>
+      <div
+        style={{
+          border: 'solid',
+          display: 'flex',
+          alignItems: 'center',
+          marginBottom: '10px',
+        }}
+      >
+        <h5 style={{ paddingRight: '10px' }}>Players:</h5>
+        <Timer />
+      </div>
+
       {playersList()}
     </div>
   );
--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from 'react';
-import Timer from '../../components/Timer';
+import { UserHub } from '../../components/User/UserHub';
 
 export const MainPage = () => {
   return (
     <Fragment>
       <h4>Welcome to Plan-It Poker!</h4>
-      <Timer />
+      <div
+        style={{
+          display: 'inline-flex',
+          alignItems: 'center',
+          padding: '10px',
+        }}
+      >
+        <div style={{ padding: '10px' }}>[Display Cards]</div>
+        <UserHub message={'Test Message'} />
+      </div>
     </Fragment>
   );
 };
