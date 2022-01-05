925efeb330285263a858f6ae43219d565471892a
Kyle F1V <f1v@kyles-mbp.mynetworksettings.com>
2022-01-05 11:35:35 -0500

    backend socket io chatbot setup

--- db/app.js
+++ db/app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
