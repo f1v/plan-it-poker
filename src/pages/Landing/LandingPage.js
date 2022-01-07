780985f0c0b2c448735e0d86c3c2693799a1f4f4
KRMeserve <krmeserve@gmail.com>
2022-01-07 11:58:06 -0500

    Landing Page initial styling

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -1,15 +1,18 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../context/UserContext';
-import { LoginModal } from '../Login/LoginPage';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import { LoginModal } from "../Login/LoginPage";
+import "./LandingPage.css";
 
 export const LandingPage = () => {
   const { userId } = useContext(UserContext);
 
   const navigate = useNavigate();
   return (
-    <>
+    <div className="background">
+      <h1 className="header">Welcome to Solar System Poker</h1>
       <button
+        className="button"
         onClick={() => {
           navigate(`/${userId}`);
         }}
@@ -17,6 +20,6 @@ export const LandingPage = () => {
         Create a Room
       </button>
       <LoginModal show={!userId} />
-    </>
+    </div>
   );
 };
